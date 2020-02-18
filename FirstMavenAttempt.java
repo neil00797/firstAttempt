c704e0c4019cd346a5002a9eb7c70f0fa57b0719
Brandon-Villet <villetbrandon@gmail.com>
2020-02-18 21:57:41 +0200

    comments  added

--- FirstMavenAttempt.java
+++ FirstMavenAttempt.java
@@ -4,12 +4,12 @@ public class FirstMavenAttempt{
     static Scanner input = new Scanner(System.in);
 
     public static void FirstMavenAttempt(String[] args) {
-        System.out.println("Enter Num 1: ");
+        System.out.println("Enter Num 1: ");// First number for addition
         double num1 = input.nextLine();
-        System.out.println("Enter Num 2: ");
+        System.out.println("Enter Num 2: ");//Second number for addition
         double num2 = input.nextLine();
         double total = num1 + num2;
 
-        System.out.println(num1 + " + " + num2 " = " total);
+        System.out.println(num1 + " + " + num2 " = " total); // Adition calculation
     }
 }
